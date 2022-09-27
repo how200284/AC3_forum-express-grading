715a6e2d437fb85828b866fdc029b9c92fc12376
how200284 <how200284@gmail.com>
2022-09-27 18:33:39 +0800

    feat: feat: add admin-services

--- controllers/apis/restaurant-controller.js
+++ controllers/apis/restaurant-controller.js
@@ -1,8 +1,8 @@
 const restaurantServices = require('../../services/restaurant-services')
 
 const restaurantController = {
-  getRestaurants: (req, res, next) => [
+  getRestaurants: (req, res, next) => {
     restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.json(data))
-  ]
+  }
 }
 module.exports = restaurantController
